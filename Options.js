e43cb1e21f128f192a9ba0bf09b2ebfd09aba60c
Tyvan <onyxichoosegaming@gmail.com>
2019-02-27 23:02:01 -0700

    Create Options.js

---
+++ Options.js
@@ -0,0 +1,16 @@
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: "BotnyxRecon",
+        password: "oauth:SECRET_PASS_HERE"
+    },
+    channels: ['OnyxRecon']
+};
+
+
+module.exports = options;
